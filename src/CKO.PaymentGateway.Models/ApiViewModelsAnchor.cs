35b43475539b0f827554d2dc5609e39c0ff4ebc0
Diogo Pinho <diogo.pinho@wolterskluwer.com>
2022-01-26 15:41:49 +0000

    add architecture tests

---
+++ src/CKO.PaymentGateway.Models/ApiViewModelsAnchor.cs
@@ -0,0 +1,8 @@
+namespace CKO.PaymentGateway.Models;
+
+/// <summary>
+/// Anchoring class for assembly scanning purposes.
+/// </summary>
+public sealed class ModelsAnchor
+{
+}
---
+++ src/CKO.PaymentGateway.Services.Abstractions/PaymentGatewayServicesAnchor.cs
@@ -0,0 +1,6 @@
+namespace CKO.PaymentGateway.Services.Abstractions;
+
+/// <summary>
+/// Anchoring class for assembly scanning purposes.
+/// </summary>
+public sealed class PaymentGatewayServicesAbstractionsAnchor { }
